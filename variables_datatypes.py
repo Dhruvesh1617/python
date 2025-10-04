494ade759558add4fe52a4e3b1da5a1653d7d011
Dhruv1617 <shettydhruvesh16@gmail.com>
2025-10-04 13:09:31 +0530

    reforcing changes

--- variables_datatypes.py
+++ variables_datatypes.py
@@ -1,1 +1,9 @@
-print("hello world from python basics")
+print("hello world from python basics \n variables and datatypes")
+
+n=10
+stringvalue="hello"
+floatvalue=10.5
+
+print ("integer value is", n)
+print ("string value is", stringvalue)
+print ("float value is", floatvalue)
